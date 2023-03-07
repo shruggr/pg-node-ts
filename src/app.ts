75ba8c018c2400aa2aa9d5f912fc9965855933d6
David Case <david.case@shruggr.cloud>
2023-03-06 20:28:55 -0500

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+const app = express();
+dotenv.config(); //Reads .env file and makes it accessible via process.env
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+async function main() {
+  await pool.connect();
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running at ${process.env.PORT}`);
+  });
+}
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.send("hi");
+});
+
+
+main().catch(console.error);
